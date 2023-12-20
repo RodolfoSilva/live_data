b115d5f08eb92ba7b499311cce5977c89d2adc2b
Rodolfo Silva <contato@rodolfosilva.com>
2023-12-20 06:43:50 -0300

    udate client

--- client/js/src/encoding/json.ts
+++ client/js/src/encoding/json.ts
@@ -1,95 +1,68 @@
-enum OpKind {
-    Render = 0,
-    SetFragment = 1,
-    SetFragmentRootTemplate = 2,
-    PatchFragment = 3,
-    SetTemplate = 4,
-    Reset = 5,
+import JSONpatch from "jsonpatch";
+
+export const unSerialize = <T = Record<string, unknown>>(
+  doc: T,
+  patch: Diff
+): T => {
+  return JSONpatch.apply_patch(doc, decompress(patch));
+};
+type Diff = [number, ...any];
+type BaseOperation = "remove";
+type FromOperation = "move" | "copy";
+type ValueOperation = "add" | "replace" | "test";
+type Operation = BaseOperation | FromOperation | ValueOperation;
+
+type BasePatch = { op: BaseOperation; path: string };
+type FromPatch = { op: FromOperation; path: string; from: string };
+type ValuePatch = { op: ValueOperation; path: string; value: string };
+
+type Patch = BasePatch | FromPatch | ValuePatch;
+
+const OPERATIONS: Record<number, Operation> = {
+  0: "remove",
+  1: "add",
+  2: "replace",
+  3: "test",
+  4: "move",
+  5: "copy",
+};
+
+function decompress(diff: Diff): Patch[] {
+  const decoded: Patch[] = [];
+
+  for (let i = 0; i < diff.length; i++) {
+    const op = diff[i];
+    const patch = {
+      op: OPERATIONS[op]!,
+      path: diff[++i] as string,
+    } as Patch;
+
+    if (["add", "replace", "test"].includes(patch.op)) {
+      (patch as ValuePatch).value = diff[++i];
+    } else if (patch.op !== "remove") {
+      (patch as FromPatch).from = diff[++i];
+    }
+
+    decoded.push(patch);
+  }
+
+  return decoded;
 }
 
 export default class JSONEncoding {
-    fragments: {[key: number]: any} = {};
-    templates: {[key: number]: any} = {};
+  __lastRender__ = { r: {} };
 
-    out: any = null;
+  get out(): Record<string, unknown> {
+    return this.__lastRender__.r;
+  }
 
-    constructor() {}
+  constructor() {}
 
-    handleMessage(ops: any): boolean {
-        let rendered = false;
+  handleMessage(diff: any): boolean {
+    let rendered = false;
 
-        ops.forEach((op: any) => {
-            let kind: OpKind = op[0];
-            switch (kind) {
-                case OpKind.Render:
-                    this.out = this.renderFragment(op[1]);
-                    rendered = true;
-                    break;
-                case OpKind.SetFragment:
-                    this.fragments[op[1]] = op[2];
-                    break;
-                case OpKind.SetFragmentRootTemplate:
-                    this.fragments[op[1]] = ["$t", op[2]].concat(op.slice(3));
-                    break;
-                case OpKind.PatchFragment:
-                    throw "unimpl";
-                    break;
-                case OpKind.SetTemplate:
-                    this.templates[op[1]] = op[2];
-                    break;
-            }
-        });
+    this.__lastRender__ = unSerialize(this.__lastRender__, diff);
 
-        return rendered;
-    }
-
-    renderFragment(fragmentId: number): any {
-        let body = this.fragments[fragmentId];
-        return this.renderBody(body);
-    }
-
-    renderTemplate(templateId: number, slots: Array<any>) {
-        let body = this.templates[templateId];
-        return this.renderBody(body, slots);
-    }
-
-    renderBody(body: any, templateSlots: Array<any> | null = null): any {
-        if (body === null) {
-            return null;
-        }
-        if (Array.isArray(body)) {
-            if (body[0] == "$r") {
-                return this.renderFragment(body[1]);
-            } else if (body[0] == "$t") {
-                let innerSlots = body.slice(2)
-                    .map((slot: any) => this.renderBody(slot, templateSlots));
-                return this.renderTemplate(body[1], innerSlots);
-            } else if (body[0] == "$s") {
-                return templateSlots[body[1]];
-            } else if (body[0] == "$e") {
-                return body[1];
-            } else {
-                return body.map((item: any) => this.renderBody(item, templateSlots));
-            }
-        }
-        if (typeof body == 'object') {
-            let out = {};
-            for (const [key, val] of Object.entries(body)) {
-                out[key] = this.renderBody(val, templateSlots);
-            }
-            return out;
-        }
-        if (typeof body == 'string') {
-            return body;
-        }
-        if (typeof body == 'number') {
-            return body;
-        }
-        if (body === true || body == false) {
-            return body;
-        }
-        console.log(body);
-        throw "unimpl";
-    }
-
-};
+    return true;
+  }
+}
--- client/js/src/index.ts
+++ client/js/src/index.ts
@@ -1,4 +1,4 @@
-import JSONEncoding from './encoding/json';
+import JSONEncoding from "./encoding/json";
 
 console.log("yay");
 
@@ -8,136 +8,134 @@ type PhxChannel = any;
 type PhxPush = any;
 
 export class Socket {
-    socket: PhxSocket;
-    opts: {};
+  socket: PhxSocket;
+  opts: {};
 
-    dvCounter: number = 0;
+  dvCounter: number = 0;
 
-    constructor(url: String, phxSocket: PhxSocketCtor, opts: {} = {}) {
-        this.socket = new phxSocket(url, opts);
-        this.opts = opts;
-    }
+  constructor(url: String, phxSocket: PhxSocketCtor, opts: {} = {}) {
+    this.socket = new phxSocket(url, opts);
+    this.opts = opts;
+  }
 
-    connect() {
-        this.socket.connect();
-    }
+  connect() {
+    this.socket.connect();
+  }
 
-    dataView(route: String, initialParams: {}, opts: {} = {}): LiveData {
-        return new LiveData(route, initialParams, opts, this);
-    }
+  dataView(route: String, initialParams: {}, opts: {} = {}): LiveData {
+    return new LiveData(route, initialParams, opts, this);
+  }
 
-    nextDvCounter(): number {
-        this.dvCounter += 1;
-        return this.dvCounter;
-    }
-
-};
+  nextDvCounter(): number {
+    this.dvCounter += 1;
+    return this.dvCounter;
+  }
+}
 
 class EventBus<Args extends Array<any>> {
-    nextId: number = 0;
-    listeners: Map<number, (...args: Args) => void> = new Map();
+  nextId: number = 0;
+  listeners: Map<number, (...args: Args) => void> = new Map();
 
-    constructor() {}
+  constructor() {}
 
-    add(listener: (...args: Args) => void): number {
-        let id = this.nextId;
-        this.nextId += 1;
-        this.listeners.set(id, listener);
-        return id;
-    }
+  add(listener: (...args: Args) => void): number {
+    let id = this.nextId;
+    this.nextId += 1;
+    this.listeners.set(id, listener);
+    return id;
+  }
 
-    remove(id: number): boolean {
-        return this.listeners.delete(id);
-    }
+  remove(id: number): boolean {
+    return this.listeners.delete(id);
+  }
 
-    call(...args: Args) {
-        this.listeners.forEach(listener => listener(...args));
-    }
+  call(...args: Args) {
+    this.listeners.forEach((listener) => listener(...args));
+  }
 }
 
 export enum RejoinPolicy {
-    Once,
-    Persist,
-};
+  Once,
+  Persist,
+}
 
 enum LiveDataState {
-    ChannelJoining = "channel_joining",
-    Joining = "joining",
-    Active = "active",
-    Terminal = "terminal",
-};
+  ChannelJoining = "channel_joining",
+  Joining = "joining",
+  Active = "active",
+  Terminal = "terminal",
+}
 
 export class LiveData {
-    rejoinPolicy: RejoinPolicy;
+  rejoinPolicy: RejoinPolicy;
 
-    socket: Socket;
+  socket: Socket;
 
-    channel: PhxChannel;
-    joinPush: PhxPush;
+  channel: PhxChannel;
+  joinPush: PhxPush;
 
-    onState: EventBus<[LiveDataState]> = new EventBus();
-    _state: LiveDataState = LiveDataState.ChannelJoining;
-    set state(state: LiveDataState) {
-        this._state = state;
-        this.onState.call(this._state);
-    }
-    get state(): LiveDataState {
-        return this._state;
-    }
+  onState: EventBus<[LiveDataState]> = new EventBus();
+  _state: LiveDataState = LiveDataState.ChannelJoining;
+  set state(state: LiveDataState) {
+    this._state = state;
+    this.onState.call(this._state);
+  }
+  get state(): LiveDataState {
+    return this._state;
+  }
 
-    onData: EventBus<[any]> = new EventBus();
-    get data(): any {
-        return this.encoding.out;
-    }
+  onData: EventBus<[any]> = new EventBus();
+  get data(): any {
+    return this.encoding.out;
+  }
 
-    encoding: JSONEncoding;
+  encoding: JSONEncoding;
 
-    constructor(route: String, initialParams: {}, opts: {}, socket: Socket) {
-        this.rejoinPolicy = opts["rejoinPolicy"] || RejoinPolicy.Persist;
+  constructor(route: String, initialParams: {}, opts: {}, socket: Socket) {
+    this.rejoinPolicy = opts["rejoinPolicy"] || RejoinPolicy.Persist;
 
-        this.socket = socket;
+    this.socket = socket;
 
-        this.encoding = new JSONEncoding();
+    this.encoding = new JSONEncoding();
 
-        let topic = "dv:c:" + this.socket.nextDvCounter();
-        // TODO params
-        this.channel = this.socket.socket.channel(topic, {"r": route});
+    let topic = "dv:c:" + this.socket.nextDvCounter();
+    // TODO params
+    this.channel = this.socket.socket.channel(topic, { r: route });
 
-        this.channel.onError(() => {});
-        this.channel.onClose(() => {});
+    this.channel.onError(() => {});
+    this.channel.onClose(() => {});
 
-        this.channel.on("o", (payload: {}) => {
-            console.log(payload);
+    this.channel.on("o", (payload: {}) => {
+      console.log(payload);
 
-            let rendered = this.encoding.handleMessage(payload["o"]);
+      let rendered = this.encoding.handleMessage(payload["o"]);
 
-            if (this.state == LiveDataState.ChannelJoining) {
-                this.state = LiveDataState.Active;
-            }
+      if (this.state == LiveDataState.ChannelJoining) {
+        this.state = LiveDataState.Active;
+      }
 
-            if (rendered) {
-                this.onData.call(this.data);
-            }
-        });
+      if (rendered) {
+        this.onData.call(this.data);
+      }
+    });
 
-        this.joinChannel();
-    }
+    this.joinChannel();
+  }
 
-    joinChannel() {
-        this.joinPush = this.channel.join();
-        this.joinPush.receive("ok", ({messages}) => {
-            this.state = LiveDataState.ChannelJoining;
-        });
-        this.joinPush.receive("error", ({reason}) => {
-            this.state = LiveDataState.Terminal;
-        });
-        this.joinPush.receive("timeout", () => {
-            this.state = LiveDataState.ChannelJoining;
-        });
-    }
+  joinChannel() {
+    this.joinPush = this.channel.join();
+    this.joinPush.receive("ok", ({ messages }) => {
+      this.state = LiveDataState.ChannelJoining;
+    });
+    this.joinPush.receive("error", ({ reason }) => {
+      this.state = LiveDataState.Terminal;
+    });
+    this.joinPush.receive("timeout", () => {
+      this.state = LiveDataState.ChannelJoining;
+    });
+  }
 
-    pushEvent(data: any) {
-        return this.channel.push("e", {d: data});
-    }
-
-};
+  pushEvent(data: any) {
+    return this.channel.push("e", { d: data });
+  }
+}
