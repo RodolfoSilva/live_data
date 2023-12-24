e56585a27a5a56dacdefd4c334302ab2b77b3be2
Rodolfo Silva <contato@rodolfosilva.com>
2023-12-24 16:38:21 -0300

    handle event from root data

--- lib/live_data/channel.ex
+++ lib/live_data/channel.ex
@@ -65,8 +65,8 @@ defmodule LiveData.Channel do
     {:stop, {:shutdown, :closed}, state}
   end
 
-  def handle_info(%Message{event: "e", payload: [event, payload | _]}, state) do
-    {:ok, socket} = state.view.handle_event(event, payload || %{}, state.socket)
+  def handle_info(%Message{event: "e", payload: %{"e" => event} = payload}, state) do
+    {:ok, socket} = state.view.handle_event(event, payload["p"] || %{}, state.socket)
     state = %{state | socket: socket}
     state = render_view(state)
     {:noreply, state}
